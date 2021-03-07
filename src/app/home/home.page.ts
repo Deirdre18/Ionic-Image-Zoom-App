ed1214b35e0fab5557ee58682d3e8411f6ae1c5a
Deirdre18 <deirdreweldon18@gmail.com>
2021-03-07 03:58:07 +0000

    modified home.page.scss, ts and image-modal.page.scss

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,30 +1,67 @@
-import { Component, ChangeDetectorRef } from '@angular/core';
-import { ImageModalPage } from '../image-modal/image-modal.page';
-import { ModalController, IonSlides } from '@ionic/angular';
+import { ChangeDetectorRef, Component } from "@angular/core";
+import { IonSlides, ModalController } from "@ionic/angular";
+import { ImageModalPage } from "../image-modal/image-modal.page";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss']
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
   sliderOpts = {
     zoom: false,
     slidesPerView: 1.5,
     spaceBetween: 20,
-    centeredSlides: true
+    centeredSlides: true,
   };
 
-  constructor(private modalController: ModalController, private changeDetectorRef: ChangeDetectorRef) { }
- 
+  zoomActive = false;
+  zoomScale = 1;
+
+  sliderZoomOpts = {
+    allowSlidePrev: false,
+    allowSlideNext: false,
+    zoom: {
+      maxRatio: 5,
+    },
+    on: {
+      zoomChange: (scale, imageEl, sideEl) => {
+        this.zoomActive = true;
+        this.zoomScale = scale / 5;
+        this.changeDetectorRef.detectChanges();
+      },
+    },
+  };
+
+  constructor(
+    private modalCtrl: ModalController,
+    private changeDetectorRef: ChangeDetectorRef
+  ) {}
+
   async openPreview(img) {
-    const modal = await this.modalController.create({
+    const modal = await this.modalCtrl.create({
       component: ImageModalPage,
-      cssClass: 'transparent-modal',
+      cssClass: "transparent-modal",
       componentProps: {
-        img
-      }
+        img,
+      },
     });
     modal.present();
   }
+
+  touchStart(card) {
+    card.el.style["z-index"] = 11;
+  }
+
+  async touchEnd(zoomslides: IonSlides, card) {
+    const slider = await zoomslides.getSwiper();
+    const zoom = slider.zoom;
+    zoom.out();
+
+    this.zoomActive = false;
+    card.el.style["z-index"] = 9;
+    this.changeDetectorRef.detectChanges();
+  }
 }
+
+ 
