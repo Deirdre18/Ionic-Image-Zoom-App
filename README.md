dc4b3cda3b65aed63c82212fc2b5882692d9b49a
Deirdre18 <deirdreweldon18@gmail.com>
2021-03-07 02:12:06 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# ionic-capacitor-Image-Gallery-Zoom-App
+
+## Credits
+
+# Sources of Code
+https://ionicacademy.com/ionic-image-zoom-guide/
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { ImageModalPage } from '../image-modal/image-modal.page';
+import { ModalController, IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  sliderOpts = {
+    zoom: false,
+    slidesPerView: 1.5,
+    spaceBetween: 20,
+    centeredSlides: true
+  };
 
-  constructor() {}
-
+  constructor(private modalController: ModalController, private changeDetectorRef: ChangeDetectorRef) { }
+ 
+  async openPreview(img) {
+    const modal = await this.modalController.create({
+      component: ImageModalPage,
+      cssClass: 'transparent-modal',
+      componentProps: {
+        img
+      }
+    });
+    modal.present();
+  }
 }
